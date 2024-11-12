103ec0adf2bfef24925f9c2a99b6f0dcc68cfe81
민우 <108320084+minu1117@users.noreply.github.com>
2024-11-12 20:29:08 +0900

    모든 스테이지 포탈 추가 (보스 스테이지는 임시) 및 포탈 테스트 완료

--- Assets/Scripts/Map/StartStage.cs
+++ Assets/Scripts/Map/StartStage.cs
@@ -4,6 +4,13 @@ public class StartStage : MonoBehaviour
 {
     [SerializeField] private Map map;
     [SerializeField] private Light directionalLight;
+    [SerializeField] private Transform startPos;
+
+    public void Awake()
+    {
+        Zed.Instance.GetMoveController().GetAgent().Warp(startPos.transform.position);
+        Zed.Instance.gameObject.transform.position = startPos.transform.position;
+    }
 
     public void SetActiveLight(bool set)
     {
